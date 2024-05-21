17db73ec34cb5409ccbd852d9fb56fdbfd1ccca0
lachlan-caldwell <33190692+lachlan-caldwell@users.noreply.github.com>
2024-05-21 17:02:43 +0100

    added Employee class

--- DataProjectSolution/src/main/java/org/example/Main.java
+++ DataProjectSolution/src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee milan = new Employee("647173,Mr.,Milan,F,Krawczyk,M,milan.krawczyk@hotmail.com,4/4/1980,1/19/2012,123681");
+        Employee jeff = milan.createEmployee("647173,Mr.,Milan,F,Krawczyk,M,milan.krawczyk@hotmail.com,4/4/1980,1/19/2012,123681");
+        System.out.println(milan.toString());
+        System.out.println(jeff.toString());
     }
 }
