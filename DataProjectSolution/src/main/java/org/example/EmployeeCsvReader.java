2924f2eb448762047ffbb23d12f2b1a921820bfa
lachlan-caldwell <33190692+lachlan-caldwell@users.noreply.github.com>
2024-05-23 15:53:43 +0100

    Logger added

--- DataProjectSolution/src/main/java/org/example/EmployeeCsvReader.java
+++ DataProjectSolution/src/main/java/org/example/EmployeeCsvReader.java
@@ -5,13 +5,20 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class EmployeeCsvReader {  
-    private static final String filePath = "src/main/resources/test_employees.csv";
+    private static final String filePath = "src/main/resources/employees.csv";
+    public static final Logger LOGGER = Logger.getLogger(EmployeeCsvReader.class.getName());
+    public static ArrayList<String> faultyLines = new ArrayList<>();
     
     public static ArrayList<String> readFileLines() {
 
         ArrayList<String> arr = new ArrayList<>();
+        setUpFileHandler();
 
         try (
                 BufferedReader reader = Files.newBufferedReader(Paths.get(filePath));) {
@@ -57,10 +64,30 @@ public class EmployeeCsvReader {
             if (isLineValid(line)) {
                 employees.add(createEmployee(line));
             }
-            //else
+            else { //add the line to an ArrayList<String> of faulty lines, and log the error.
+                logFaultyLine(line);
+            }
         }
-        
+
         return employees;
     }
+
+    private static void setUpFileHandler(){
+        LOGGER.setUseParentHandlers(false);
+        FileHandler handler = null;
+        try {
+            handler = new FileHandler("src/main/resources/faultyLines.log", true);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        handler.setLevel(Level.WARNING);
+        LOGGER.addHandler(handler);
+    }
+
+    private static void logFaultyLine(String line) {
+        faultyLines.add(line);
+        LOGGER.log(Level.WARNING, "Faulty line: " + line);
+    }
+
 
 }
--- DataProjectSolution/src/main/java/org/example/Main.java
+++ DataProjectSolution/src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        EmployeeCsvReader.readEmployees();
+        System.out.println(EmployeeCsvReader.faultyLines.size());
     }
 }
